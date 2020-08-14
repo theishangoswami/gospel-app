cf888a630294cb5ed198acbe5007ffc6906c67b9
Himanshu Sharma <ranubhardwaj89@gmail.com>
2020-08-15 00:00:53 +0530

    Color Theme Added

---
+++ lib/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  
+  AppTheme._();
+  static const Color blue = Color(0xff0a0548);
+  static const Color green = Color(0xFF00ffa4);
+  static const Color lightBlue = Color(0xff316bf8);
+}
