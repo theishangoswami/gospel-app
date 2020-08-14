0a0d6e140d0901eea5e824dfd9a0b0d84c934b2f
Himanshu Sharma <ranubhardwaj89@gmail.com>
2020-08-15 00:00:28 +0530

    [Improvement] Add Gospel Screen UI

--- lib/add_gospel/create_gospel.dart
+++ lib/add_gospel/create_gospel.dart
@@ -1,94 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:gospel/app_theme.dart';
 
 class CreateGospel extends StatelessWidget {
+
+  List rules = [
+    'With great power comes great responsibilty.',
+    'Be polite and do not abuse other!',
+    'Zero tolerance policy on posting people\'s personal information',
+    'Only post relevant topics in gospel',
+    'Enjoy your freedom of expression and restrain inappropriate behavior',
+  ];
+
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: AppTheme.green,
+      resizeToAvoidBottomPadding: false,
         appBar: AppBar(
-          backgroundColor: const Color(0xFF00ffa4),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
           title: Text(
-            'Create Gospel',
+            'CREATE GOSPEL',
             style: TextStyle(
-              color: Color(0xFF0a0548),
+              color: AppTheme.blue,
+              letterSpacing: 1
             ),
           ),
           leading: IconButton(
             onPressed: () => Navigator.pop(context),
-            icon: Icon(Icons.arrow_back),
+            icon: Icon(Icons.arrow_back_ios),
             iconSize: 30.0,
-            color: Color(0xff0a0548),
+            color: AppTheme.blue,
           ),
         ),
-        body: SingleChildScrollView(
+        body: Container(
+          height: height,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15),
-                child: Text(
-                  "Rules to follow",
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.w500,
-                    color: Color(0xFF000000),
+                padding: EdgeInsets.symmetric(horizontal: 35,vertical: 10),
+                child: Container(
+                  height: height*0.25,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    color: AppTheme.blue.withOpacity(0.8)
+                  ),
+                  child: TextFormField(
+                    maxLines: 10,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white
+                    ),
+                    cursorColor: AppTheme.blue,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    ),
                   ),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20.0,
-                ),
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15),
                 child: Text(
-                  " • With great power comes great responsibilty. \n\n • Be polite and do not abuse other! \n\n • Zero tolerance policy on posting people's personal information \n\n • Only post relevant topics in gospel \n\n • Enjoy your freedom of expression and restrain inappropriate behavior \n",
+                  "RULES TO FOLLOW",
                   style: TextStyle(
                     fontSize: 20.0,
-                    // fontWeight: FontWeight.w500,
-                    color: Color(0xFF000000),
+                    fontWeight: FontWeight.w500,
+                    color: AppTheme.blue,
                   ),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Container(
-                  height: 130,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 3,
-                    ),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20.0,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16.0,
-                ),
-                child: Center(
-                  child: RaisedButton(
-                      color: Color(0xff0a0548),
-                      textColor: Colors.white,
-                      child: Row(
-                        children: <Widget>[
-                          Text(
-                            "Post Gospel",
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.w500,
+                child: Column(
+                  children: [
+                    for(int i=0;i<rules.length;i++)
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 2),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.check_circle,
+                              size: 17,
+                              color: AppTheme.blue
                             ),
-                          ),
-                          SizedBox(width: 15),
-                          Icon(Icons.send),
-                        ],
+                            SizedBox(width: width*0.02,),
+                            Expanded(
+                              child: Text(
+                                rules[i],
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: AppTheme.blue
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                      padding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 90),
-                      onPressed: () => Navigator.pop(context)),
-                ),
+                    SizedBox(height: height*0.12,),
+                    Container(
+                      width: height*0.08,
+                      height: height*0.08,
+                      decoration: BoxDecoration(
+                        color: AppTheme.blue,
+                        shape: BoxShape.circle
+                      ),
+                      child: Icon(
+                        Icons.arrow_forward_ios,
+                        size: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: height*0.01,),
+                    Text(
+                      'ADD GOSPEL',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: AppTheme.blue
+                      ),
+                    ),
+                  ],
+                )
               ),
             ],
           ),
-        ));
+        )
+      );
   }
 }
